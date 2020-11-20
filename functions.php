ef97f7b3cecd4971a3c018489f0cb20c44304291
aliraza114 <aliraza3569@gmail.com>
2020-11-20 11:26:54 +0500

    widgets and pages added

--- functions.php
+++ functions.php
@@ -38,4 +38,32 @@ function janigaga_register_scripts(){
 }
 add_action( 'wp_enqueue_scripts', 'janigaga_register_scripts');
 
+function janigaga_widget_areas() {
+    register_sidebar(
+        array(
+            'before_title' => '',
+            'after_title' => '',
+            'before_widget' => '<ul class="social-list list-inline py-3 mx-auto">',
+            'after_widget' => '</ul>',
+            'name' => 'Sidebar Area',
+            'id' => 'sidebar-1',
+            'description' => 'Sidebar Widget Area',
+        )
+    );
+
+    register_sidebar(
+        array(
+            'before_title' => '',
+            'after_title' => '',
+            'before_widget' => '<ul class="social-list list-inline py-3 mx-auto">',
+            'after_widget' => '</ul>',
+            'name' => 'Footer Area',
+            'id' => 'footer-1',
+            'description' => 'Footer Widget Area',
+        )
+    );
+}
+
+add_action('widgets_init', 'janigaga_widget_areas');
+
 ?>
--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -1,15 +1,19 @@
 <div class="container">
-    <header class="content-header">
-        <div class="meta mb-3"><span class="date"><?php the_date(); ?></span>
-        <?php the_tags('<span class="tag"><i class="fa fa-tag"></i>', '</span><span class="tag"><i class="fa fa-tag"></i>', '</span>'); ?>
-        <span class="comment"><a href="#comments"><i class='fa fa-comment'>
-
-        </i> <?php comments_number(); ?> </a></span></div>
-    </header>
-    <?php 
-        the_content();
-    ?>
-    <?php 
-        comments_template();
-    ?>
+<div class="post mb-5">
+    <div class="media">
+        <img class="mr-3 img-fluid post-thumb d-none d-md-flex" 
+        src="<?php the_post_thumbnail('thumbnail');?>" alt="image">
+        <div class="media-body">
+            <h3 class="title mb-1"><a class="more-link" href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a></h3>
+            <div class="meta mb-1"><span class="date"> <?php the_date(); ?> </span>
+            <span class="comment"><a href="#"> <?php comments_number(); ?> </a></span></div>
+            <div class="intro">
+                <?php 
+                    the_excerpt();
+                ?>
+            </div>
+            <a class="more-link" href="<?php the_permalink(); ?>">Read more &rarr;</a>
+        </div><!--//media-body-->
+    </div><!--//media-->
+</div>
 </div>
